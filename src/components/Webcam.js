e7f617ae5b448bf70388fc81c64a53c49e852457
Roman Stankiewicz <roman.stan3@gmail.com>
2018-01-03 14:21:13 +0000

    qr camera

--- src/components/Webcam.js
+++ src/components/Webcam.js
@@ -4,14 +4,33 @@ import { Link } from 'react-router-dom'
 import LinkButton from './modules/LinkButton'
 import Camera from 'react-camera';
 
-
+import QrReader from 'react-qr-reader'
 
 
 export default class WebcamComponent extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      delay: 300,
+      result: 'No result',
+    }
+    this.handleScan = this.handleScan.bind(this)
+  }
+  handleScan(data){
+    console.log("data",data)
+    if(data){
+      this.setState({
+        result: data,
+      })
+    }
+  }
+  handleError(err){
+    console.error(err)
+  }
   render() {
     const width = window.innerWidth
     const height = window.innerHeight
-    console.log("height",height)
     return (
       <div className='webcam'>
         <div className='webcamWrap'>
@@ -20,13 +39,22 @@ export default class WebcamComponent extends Component {
             height={height - 70 - 80}
             audio={false}/> */}
 
-            <Camera
+            {/* <Camera
               // style={style.preview}
               // ref={(cam) => {
               //   this.camera = cam;
               // }}
             >
-            </Camera>
+            </Camera> */}
+
+            <div className='viewfinder'></div>
+            <QrReader
+              delay={this.state.delay}
+              onError={this.handleError}
+              onScan={this.handleScan}
+              showViewFinder={false}
+              style={{ width: '100%'}}
+              />
         </div>
         <div className='capture'>Capture</div>
         <Link to='ShopPage'>Back to shop</Link>
